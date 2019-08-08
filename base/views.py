70a3deb8b8d7e9c31026520e28fb888ed1209a4a
Vitaly <lavr3x@rambler.ru>
2019-08-08 11:53:22 +0800

    controlers for first urls

--- base/views.py
+++ base/views.py
@@ -2,5 +2,21 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+class Index(View):
+
+    def get(self, request, *args, **kwargs):
+        return render(request, 'index.html')
+
+    def post(self, request, *args, **kwargs):
+        pass
+
+class PS(View):
+
+    def get(self, request, *args, **kwargs):
+        return render(request, 'PS.html')
+
+    def post(self, request, *args, **kwargs):
+        pass
--- vinat/urls.py
+++ vinat/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.urls import path
+
+import base.views as views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('', views.Index.as_view()),
+    path('ps', views.PS.as_view()),
 ]
